b36d849e9114c0096aaa5482d9764ea08b7da1ce
Mike Goren <mike@mortalwombat.net>
2024-02-23 16:23:16 -0800

    add script to number lessons from day of week

--- scripts/numberLessons.js
+++ scripts/numberLessons.js
@@ -1,8 +1,9 @@
-// give lesson Fidgetech numbers
+// give lesson numbering system
 
-import { existsSync, readFileSync, readdirSync, writeFileSync, statSync } from 'fs';
+import { existsSync, readFileSync, readdirSync, statSync, unlinkSync } from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
+import { titleToId, generatePrefix, writeMarkdown } from './utils.js';
 const docsPath = join(process.cwd().replace(/(scripts|docs)/, ''), 'docs');
 const dayMapping = { 'weekend': 0, 'monday': 1,'tuesday': 2, 'wednesday': 3, 'thursday': 4, 'friday': 5, 'saturday': 6 };
 
@@ -20,7 +21,7 @@ function generateSidebarForDirectory(directoryPath) {
     console.log('\nSection', sectionNumber);
     const filenames = readdirSync(directoryPath).filter(fileName => fileName.endsWith('.md')).sort();
     let segmentCounter = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
-    for (const filename of filenames) {
+    for (const [index, filename] of filenames.entries()) {
       const filePath = join(directoryPath, filename);
       const fileContent = readFileSync(filePath, 'utf8');
       const { content, data } = matter(fileContent, { preserve: true });
@@ -29,7 +30,18 @@ function generateSidebarForDirectory(directoryPath) {
       const lessonNumber = segmentCounter[segment];
       const emoji = data.title.includes('✏️') ? '✏️' : '📓';
       const title = `${emoji} ${courseNumber}.${sectionNumber}.${segment}.${lessonNumber} ${data.title.replace(/(✏️ |📓 )/, '')}`;
-      console.log(title);
+      data.title = title;
+      console.log(data.title);
+      const prefix = generatePrefix(index);
+      const newFilename = `${prefix}-${titleToId(title)}.md`;
+      const newFilePath = join(directoryPath, newFilename);
+      if (filename.split('-').slice(1).join('-') === newFilename.split('-').slice(5).join('-')) {
+        console.log(`renaming ${filename} to ${newFilename}`);
+        // writeMarkdown(newFilePath, content, data);
+        // unlinkSync(filePath);
+      } else {
+        console.log('filenames do not match', filename, newFilename);
+      }
     }
   }
 }
